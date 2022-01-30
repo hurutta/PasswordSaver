038d5bc14e0b217c6b2c341457c40d98d61c6f2a
Jawad <abidmohammadjawad@gmail.com>
2022-01-30 20:14:03 +0600

    Create RESTful web services

--- backend/src/main/java/com/example/PasswordSaver/controller/PasswordSaverController.java
+++ backend/src/main/java/com/example/PasswordSaver/controller/PasswordSaverController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @CrossOrigin(origins = "http://localhost:8080")
 
 @RestController
